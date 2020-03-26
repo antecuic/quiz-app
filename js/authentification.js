e813038e1a2e43e6ecafcda141ac39b1f88713e6
Ante Čuić <ante.tg@hotmail.com>
2020-03-26 23:57:54 +0100

    auth update

--- js/authentification.js
+++ js/authentification.js
@@ -1,34 +1,19 @@
 const signupForm = document.querySelector('#signup-form');
+const btnSignup = document.querySelector('#btn-signup');
 const loginForm = document.querySelector('#login-form');
 const btnLogout = document.querySelector('#logout');
-let loggedUsername = null;
-  
+const modal = document.querySelector('.modal');
+
 //state changed
 auth.onAuthStateChanged(user => {
     if(user) {
         //user logout
+        btnLogout.style.display = 'inline-block';
         btnLogout.addEventListener('click', () => auth.signOut());
 
-        dataBase.collection("users").get().then(querySnapshot => {
-            querySnapshot.forEach(doc => {
-                //getting logged user username from firestore database 
-                Object.keys(doc.data().newUser).forEach(key => {
-
-                    if(user.email === doc.data().newUser[key]) {
-                        loggedUsername = doc.id
-                        console.log('Logged username => ' + loggedUsername)
-                    };
-                });
- 
-            });
-        });
-           
     } else {
-        loggedUsername = null;
-        console.log('user logged out');
-        console.log('Logged username => ' + loggedUsername);
+        btnLogout.style.display = 'none';
     }
-
    
 });
 
@@ -37,21 +22,42 @@ loginForm.addEventListener('submit', e => {
     e.preventDefault()
     const email = loginForm['emailLogin'].value;
     const password = loginForm['passwordLogin'].value;
-    auth.signInWithEmailAndPassword(email, password).then(loginForm.reset());
+
+    if (email === '' || password === '') {
+        return;
+    }
+
+    closeModal();
+
+    auth.signInWithEmailAndPassword(email, password)
+        .then(loginForm.reset())
+        .catch(error => alert(error.message));
 });
 
 //signup
 signupForm.addEventListener('submit', e => {
+
     e.preventDefault()
     const usrName = signupForm['usernameSignup'].value;
     const email = signupForm['emailSignup'].value;
-    const password = signupForm['passwordSignup'].value;
+    const password = signupForm['passwordSignup'].value;    
+
+    if((usrName === '') || (email === '') || (password === '')) {
+        return;
+    }
 
     const newUser = {
         username: usrName,
         mail: email,
         password: password
     }
+
+    closeModal();
+
     auth.createUserWithEmailAndPassword(email, password)
-        .then(dataBase.collection('users').doc(usrName).set({ newUser }))
+        .then(result => {
+            return result.user.updateProfile({displayName: usrName})
+        })
+        .then(signupForm.reset())
+        .catch(error => alert(error.message))
 });
